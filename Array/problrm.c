581225fc1339b4e97d6579730c053a5a3c7335a1
rahulbaraiya8388 <rahulbaraiya8388@gmail.com>
2023-02-14 17:07:12 +0530

    R

--- Array/problrm.c
+++ Array/problrm.c
@@ -1,22 +1,23 @@
 //  print the number in reverse order
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a[9]={34,56,54,32,67,89,90,32,21};
+int main()
+{
+    int a[9] = {34, 56, 54, 32, 67, 89, 90, 32, 21};
 
     int i;
 
-    for(i=0;i<9;i++){
+    for (i = 0; i < 9; i++)
+    {
 
-        printf("%d ",a[i]);
-
+        printf("%d ", a[i]);
     }
-     printf("\n");
+    printf("\n");
 
-    for(i=8;i>=0;i--){
-        printf("%d ",a[i]);
+    for (i = 8; i >= 0; i--)
+    {
+        printf("%d ", a[i]);
     }
 
-    
     return 0;
 }
